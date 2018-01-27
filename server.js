42d32e637f18ed1a6aee61021deec72e36f19b78
Glitch (fcodec-headerparser) <none>
2018-01-27 20:45:37 +0000

    💝🏟 Updated with Glitch

--- server.js
+++ server.js
@@ -3,12 +3,6 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('public'));
-
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-
 app.get("/api/whoami", function (req, res) {
   var ipaddress = req.headers['x-forwarded-for'].split(',')[0]
   var language = req.headers['accept-language'].split(',')[0]
